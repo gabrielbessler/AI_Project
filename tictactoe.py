5cc55c443e39c1ce2522b8250351292ee4462395
Gabriel Bessler <gabrielbessler@gmail.com>
2019-11-06 21:13:58 -0800

    Initial NN To Guess Value Function

--- tictactoe.py
+++ tictactoe.py
@@ -1,15 +1,65 @@
 import copy
-import torch
+
+def checkGameOver(board, logger): 
+    for i in range(3):
+        inARow_V = True
+        inARow_H = True
+        startingPiece_H = board[i][0]
+        startingPiece_V = board[0][i]
+        for j in range(1, 3):
+            if inARow_H and board[i][j] != startingPiece_H:
+                inARow_H = False
+            if inARow_V and board[j][i] != startingPiece_V:
+                inARow_V = False
+                break 
+            
+        if inARow_V and startingPiece_V != 0:
+            logger.d(f"Vertical: {i}")
+            return True, startingPiece_V
+
+        if inARow_H and startingPiece_H != 0:
+            logger.d(f"Horizontal: {i}")
+            return True, startingPiece_H
+    
+    diagOne = True
+    diagTwo = True
+    diagOneStarting = board[0][0]
+    diagTwoStarting = board[2][2]
+    for i in range(1, 3):
+        if diagOne and board[i][i] != diagOneStarting:
+            diagOne = False
+        if diagTwo and board[2-i][2-i] != diagTwoStarting:
+            diagTwo = False
+
+    if diagOne and diagOneStarting!= 0: 
+        logger.d(f"Diag One")
+        return True, diagOneStarting
+    
+    if diagTwo and diagTwoStarting!= 0: 
+        logger.d(f"Diag Two")
+        return True, diagTwoStarting
+
+    return False, 0 
 
 class Log:
-    def __init__(self, output):
-        self.output = output
+    def __init__(self, output_level):
+        # 0 = Everything
+        # 1 = Debug 
+        # 2 = Fatal Only 
+        self.output = output_level
 
-    def log(self, s):
-        if self.output:
+    def e(self, s):
+        if self.output <= 0:
             print(s) 
-        else: 
-            pass
+
+    def d(self, s): 
+        if self.output <= 1:
+            print(s)
+
+    def f(self, s): 
+        if self.output <= 2: 
+            print(s)
+
 
 class Game: 
     def __init__(self, agentOne, agentTwo, logger):
@@ -28,47 +78,11 @@ class Game:
         return s
 
     def reset(self): 
-        self.logger.log("Resetting...")
+        self.logger.e("Resetting...")
         self.currPlayer = 0
         self.currTurn = 0
         self.board = copy.deepcopy(self.__init_state)
 
-    def checkGameOver(self): 
-        for i in range(self.dim):
-            inARow_V = True
-            inARow_H = True
-            startingPiece_V = self.board[i][0]
-            startingPiece_H = self.board[0][i]
-            for j in range(1, self.dim):
-                if inARow_V and self.board[i][j] != startingPiece_V:
-                    inARow_V = False
-                if inARow_H and self.board[j][i] != startingPiece_H:
-                    inARow_H = False
-                    break 
-                
-            if inARow_V and startingPiece_V != 0:
-                return True, startingPiece_V
-
-            if inARow_H and startingPiece_H != 0:
-                return True, startingPiece_H
-        
-        diagOne = True
-        diagTwo = True
-        diagOneStarting = self.board[0][0]
-        diagTwoStarting = self.board[self.dim-1][self.dim-1]
-        for i in range(1, self.dim):
-            if diagOne and self.board[i][i] != diagOneStarting:
-                diagOne = False
-            if diagTwo and self.board[self.dim-1-i][self.dim-1-i] != diagTwoStarting:
-                diagTwo = False
-
-        if diagOne and diagOneStarting!= 0: 
-            return True, diagOneStarting
-        
-        if diagTwo and diagTwoStarting!= 0: 
-            return True, diagTwoStarting
-
-        return False, 0 
 
     def makeMove(self, action):
         # Assumes the move is valid
@@ -76,9 +90,12 @@ class Game:
         self.board[action[0]][action[1]] = pieceToPlay
         return True
 
+    def checkGameOver(self):
+        checkGameOver(self.board, self.logger)
+
     def play(self):
         while self.checkGameOver() != True and self.currTurn != 9:
-            self.logger.log(self)
+            self.logger.e(self)
             if self.currPlayer == 0:
                 move = self.agentOne.getMove(self.board)
                 self.currPlayer = 1
@@ -87,19 +104,25 @@ class Game:
                 self.currPlayer = 0
             isValid = self.makeMove(move)
             if not isValid: 
-                self.logger.log("not a valid move")
+                self.logger.f("not a valid move")
             self.currTurn += 1
-        self.logger.log(self)
-        self.logger.log(self.checkGameOver())
+        self.logger.e(self)
+        gameResults = self.checkGameOver()
+        self.logger.d(gameResults)
+        return gameResults 
 
-class ConstAgent():
-    def __init__(self, playerNum): 
-        pass 
+class ValueAgent():
+    def __init__(self, playerNum, valueFunction): 
+        self.valueFunction = valueFunction
     
     def getMove(self, board): 
-        return (2, 2)
+        action = self.valueFunction(board)
+        return self.convert(action) 
 
-class Agent(): 
+    def convert(self, action):
+        return (action % 3, action // 3)
+
+class DumbAgent(): 
     def __init__(self, playerNum): 
         self.playerNum = playerNum
 
@@ -110,7 +133,7 @@ class Agent():
                     return (i, j)
 
 if __name__ == "__main__":
-    game = Game(Agent(0), ConstAgent(1), Log(True))
+    game = Game(DumbAgent(0), DumbAgent(1), Log(0))
     game.play()
     game.reset()
     game.play()
