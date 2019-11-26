0dddaa8e71865672286406b1766ec2acae9894e8
Sonia Sehra <soniasehra@wl-204-68.wpa.claremont.edu>
2019-11-25 16:39:15 -0800

    some more structure but lots of questions

--- MTCS.py
+++ MTCS.py
@@ -9,7 +9,8 @@ class MCTS:
     "Monte Carlo tree searcher."
 
     #c is some tunable parameter that helps select what node to progress to
-    def __init__(self, root_state, game, c=1):
+    #TODO: takes in some model. can call model.predict() and model.update()
+    def __init__(self, root_state, game, model, c=1):
         self.W = dict()  # total reward of taking action from state
         self.Q = dict() # average reward of taking action from state
         self.N = dict() # total visit count of taking action from state
@@ -20,10 +21,11 @@ class MCTS:
         #setting up the initial root state
         self.root = root_state
         self.initalize(root_state)
-        # we get this policy from the nnet, of probabilites of actions
+        # we get this policy from the nnet, of probabilites of actions given a state
         self.policy = dict()
         self.path = []
         self.game = game
+        self.model = model
 
     #must be called every time we want to evaluate with MCTS after the intialization
     def set_root(self,state):
@@ -31,6 +33,7 @@ class MCTS:
 
     def initalize(self,state):
         self.actions[root_state] = []
+        self.policy[root_state] = dict()
 
     #after intialization, this will be the MAIN function that is called
     def perform_iterations(self,numIterations):
@@ -52,11 +55,15 @@ class MCTS:
             leaf_value = terminal_value
             # we then backpropagate these results back up the tree to the root
             self.backpropagate(leaf_value)
+            # TODO: do we need this? or just calling it from the policy dictionary if backprop computes
+            # this for each node it traversed? Is backprop even doing that?
             pi = self.get_action_prob_dist()
             #TODO: get probability vector distribution from number of times perfomed actions
             #TODO update nn so that predicted value  and prob dist is not so different actual score
+            self.model.update(pi, leaf_value)
             # and probabiliy distribution for the actions taken
             # don't actually know how to integrate that here
+
         else:
             #if it is not a terminal state, then we expand the possible children of the node,
             #and take the action dictated by the neural net
@@ -80,7 +87,7 @@ class MCTS:
 
     def select_leaf(self):
         current_state = self.root
-        while not self.isleaf(current_state):
+        while not self.is_leaf(current_state):
             best_action = self.choose_action[current_state]
             #see if we taken action before, if not, add the child to the dictionary:
             if best_action not in self.child[current_state]:
@@ -116,7 +123,10 @@ class MCTS:
             total_n += self.N[state][action]
         action_values = dict()
         for action in self.actions[state]:
-            action_values[action] = self.Q[state][action] + (self.c * self.policy[action] * math.sqrt(math.log(total_n) / (1 + self.N[state][action])))
+            # for the policy, do we ask the neural net each time what it is?
+            #or do we ask once and save?
+            #or use the policy computed by backprop at the end
+            action_values[action] = self.Q[state][action] + (self.c * self.policy[state][action] * math.sqrt(math.log(total_n) / (1 + self.N[state][action])))
         return max(action_values, key=action_values.get())
 
     def expand_leaf(self, state):
@@ -132,7 +142,8 @@ class MCTS:
             self.N[state][action] = 0
             self.W[state][action] = 0
             self.Q[state][action] = 0
-        self.policy, value = #TODO nnet updates the policy and value based on this current state
+        #TODO: figure out which state's policy vector is getting updated (I think not root state)?
+        self.policy[state or root_state], value =  self.model.predict(state)#TODO nnet updates the policy and value based on this current state
         return value
 
     def backpropagate(value):
@@ -142,3 +153,11 @@ class MCTS:
             self.N[state][action] += 1
             self.W[state][action] += value
             self.Q[state][action] = self.W[state][action] / self.N[state][action]
+            #TODO: Do we update the policy here (probability of taking actions from state we traversed) as well?
+            # if so do we compute a new policy from the N values? or do something like
+            # P[state][action] = ((N-1)*P[state][action] + 1)/N  for the action we visited?
+            # and for the actions we didnt do ((N-1)*P[state][action])/N
+            # since P[state][action] could be due to neural net?
+            #
+            # or do we want to compute total N across all actions
+            # then for each action update P[state][action] = N[action]/N <- i think this one
